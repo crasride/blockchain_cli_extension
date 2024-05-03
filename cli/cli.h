a5430ea84df18068c7dc56bab9b9bef4bb3c6abc
crasride <jose.dfernandez@yahoo.fr>
2024-05-03 16:51:33 +0200

    add function mine auto

--- cli/cli.h
+++ cli/cli.h
@@ -153,4 +153,6 @@ int is_output_unspent(uint8_t output_hash[SHA256_DIGEST_LENGTH],
 int handle_info_selection(state_t *state);
 int print_unspent(llist_node_t node, unsigned int idx, void *args);
 
+bool is_number(char *string);
+int handle_mine_auto(state_t *state, int nb_block, int interval);
 # endif /* _CLI_H_ */
