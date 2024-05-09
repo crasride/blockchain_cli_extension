16ce083c8426d57fd76196c7ac73b391bdc21989
crasride <jose.dfernandez@yahoo.fr>
2024-05-09 15:18:47 +0200

    create function generate block genesis

--- blockchain/v0.3/create_genesis_block.c
+++ blockchain/v0.3/create_genesis_block.c
@@ -45,28 +45,3 @@ block_t *create_genesis_block(void)
 
 	return (genesis_block);
 }
-
-/*
-int main(void)
-{
-
-	block_t *genesis_block = create_genesis_block();
-
-	if (!genesis_block)
-	{
-		fprintf(stderr, "Error al crear el bloque génesis\n");
-		return (1);
-	}
-
-
-	printf("Hash del bloque génesis: ");
-	print_hash(genesis_block->hash);
-	printf("\n");
-
-
-	free(genesis_block);
-
-	return (0);
-}
- */
-
