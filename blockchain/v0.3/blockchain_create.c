8e26f129a28fecc71c276b4f50869c4e4743b3fc
crasride <jose.dfernandez@yahoo.fr>
2024-05-09 15:17:56 +0200

    create function generate block genesis

--- blockchain/v0.3/blockchain_create.c
+++ blockchain/v0.3/blockchain_create.c
@@ -37,16 +37,10 @@ blockchain_t *blockchain_create(void)
 		free(genesis_block);
 		return (NULL);
 	}
-	/* Initialize the block */
-	memset(genesis_block, 0, sizeof(block_t));/* Initialize the genesis block */
-	genesis_block->info.index = 0;
-	genesis_block->info.difficulty = 0;
-	genesis_block->info.timestamp = 1537578000;
-	genesis_block->info.nonce = 0;
-	memset(genesis_block->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
-	memcpy(genesis_block->data.buffer, "Holberton School", 16);
-	genesis_block->data.len = 16;
-	memcpy(genesis_block->hash, GENESIS_HASH, SHA256_DIGEST_LENGTH);
+
+	/* create genesis block*/
+	genesis_block = create_genesis_block();
+
 	/* Add the genesis block to the blockchain */
 	if (llist_add_node(blockchain->chain, genesis_block, ADD_NODE_REAR) == -1)
 	{
